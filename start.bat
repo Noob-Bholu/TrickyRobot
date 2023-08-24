537f51088060112a557f70f7ea5b2a5cdbcb53ff
Noob-Bholu <143063747+Noob-Bholu@users.noreply.github.com>
2023-08-24 20:20:46 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off 
- TITLE Bholu Robotz 
- :: Enables virtual env mode and then starts Bholu 
- env\scripts\activate.bat && py -m BholuRobotz
+ TITLE Tricky Heaven 
+ :: Enables virtual env mode and then starts TrickyHeaven 
+ env\scripts\activate.bat && py -m TrickyHeaven
