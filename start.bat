95b9650c08c5d596e2daa7da3ae08d0bf2885dc7
Noob-Bholu <143063747+Noob-Bholu@users.noreply.github.com>
2023-08-26 17:05:57 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off 
- TITLE Tricky Heaven 
- :: Enables virtual env mode and then starts TrickyHeaven 
+ TITLE TrickyRobot 
+ :: Enables virtual env mode and then starts Tricky 
  env\scripts\activate.bat && py -m TrickyRobot
